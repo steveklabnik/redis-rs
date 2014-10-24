b505a3e125246800b8b37f58168cdd9375bc447e
Armin Ronacher <armin.ronacher@active-4.com>
2014-10-24 13:31:10 +0200

    Built documentation

---
+++ implementors/collections/trait.Map.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['redis'] = ["<a class='stability Experimental' title='Experimental'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/collections/trait.Map.html' title='collections::Map'>Map</a>&lt;&amp;'a <a href='http://doc.rust-lang.org/nightly/std/primitive.str.html'>str</a>, <a class='enum' href='redis/enum.Value.html' title='redis::Value'>Value</a>&gt; for <a class='struct' href='redis/struct.InfoDict.html' title='redis::InfoDict'>InfoDict</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/collections/trait.Collection.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['redis'] = ["<a class='stability Experimental' title='Experimental'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/collections/trait.Collection.html' title='core::collections::Collection'>Collection</a> for <a class='struct' href='redis/struct.InfoDict.html' title='redis::InfoDict'>InfoDict</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
